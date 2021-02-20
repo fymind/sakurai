eae75871a6a4b37e3493238404c123a5f3c7b6b9
Anton Hlushkov <60152947+fymind@users.noreply.github.com>
2021-02-20 14:52:04 +0200

    Add files via upload

---
+++ sakurai/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Free United Computer Ark / FUCA.org</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="header2.png" type="image/png">
+  	<meta name="description" content="Software Ark Registry">
+  	<meta name="keywords" content="Software, Ark, Registry">
+  	<meta name="author" content="FUCA.org">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header"><img width="100%" src="header3.png"></img></div>
+
+	<h1><header>About</header></h1>
+	<p><i><b>FUCA.org</b> (Free United Computer Ark)</i> is a non-profit, computer software ark with small user base. If you want to share your software in <a href="static/about_registry.html">FUCA.org Registry</a> please follow this <a href="reg/index.php">link (now in development)</a>.</p>
+<footer class="fixed-bottom">
+		<img src="pow.gif"></img>
+	</footer>
+	</div>
+	
+</body>
+</html>
